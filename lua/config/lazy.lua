2427ee850a124745805c7dfa3e17b7e023b832fc
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com>
2024-09-20 11:28:39 +0200

    fix: move leader key assignment before anything else

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,12 +10,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
---
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
